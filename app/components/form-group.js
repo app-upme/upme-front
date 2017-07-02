8d522362663413fc4f6d3d2ba79a0e4850da19ec
Nicolas Lima <lima.nicolasmateus@gmail.com>
2017-07-02 01:46:04 -0400

    adicionando traduções; ajustes no modal

--- app/components/form-group.js
+++ app/components/form-group.js
@@ -1,5 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  name: null
+  name: null,
+  description: null
 });
--- app/locales/pt-br/translations.js
+++ app/locales/pt-br/translations.js
@@ -1,9 +1,19 @@
 export default {
-  "history": "Histórico",
   "components": {
     "form-group": {
-      "placeholders.name": "Nome da turma",
+      "placeholders": {
+        "name": "Nome da turma",
+        "description": "Descrição da turma"
+      },
       "buttons.submit": "Salvar turma"
+    }
+  },
+  "pages": {
+    "dashboard": {
+      "groups": "Turmas",
+      "history": "Histórico",
+      "placeholders.search": "Buscar turma",
+      "dialog.title": "Adicionar turma"
     }
   }
 };
